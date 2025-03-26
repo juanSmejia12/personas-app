9d9ec830bb0b7c3b81b806b43f481bd23ec31f18
Juan Mejia <juansebastianmejiagomez31@gmail.com>
2025-03-26 13:22:08 -0500

    Municipio List

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//comuna
 Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
 Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
 Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
 Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
 Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
 Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+
+//municipio
+Route::get('/municipios', [MunicipioController::class, 'index'])->name('municipios.index');
